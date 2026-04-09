bddf7337bd62cb0a342324be09bc595dd659ec28
santonioG <205916771+santonioG@users.noreply.github.com>
2026-04-08 22:42:54 -0400

    servicio finalizado y corriendo de forma correcta

--- src/main/java/com/duoc/LearningPlatform/controller/CourseController.java
+++ src/main/java/com/duoc/LearningPlatform/controller/CourseController.java
@@ -1,8 +1,26 @@
 package com.duoc.learningplatform.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duoc.learningplatform.model.Course;
+import com.duoc.learningplatform.service.ICourseService;
+
+@RestController
+@RequestMapping("/api/courses")
 public class CourseController { //Crea controladores que gestionan las solicitudes HTTP
                                 // y se comunican los servicios
 
-    
+    @Autowired
+    private ICourseService courseService;
+
+    @GetMapping("/activos")
+    public ArrayList<Course> listarActivos(){
+        return courseService.obtenerCursosActivosOrdenados();
+    }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO course (activo, descripcion, nombre) VALUES (true, 'Curso de Spring Boot', 'Backend I');
+INSERT INTO course (activo, descripcion, nombre) VALUES (true, 'Curso de AWS', 'DevOps');
+INSERT INTO course (activo, descripcion, nombre) VALUES (false, 'Curso Antiguo', 'Java Base');
